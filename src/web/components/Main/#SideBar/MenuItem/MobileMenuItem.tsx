932521cec1ab5605110a4dece3ebbd4db72a78ca
geshimi <clairobscour.gmail.com>
2021-07-02 16:38:37 +0300

    feat: sidebar submenu mobile started implemented

--- src/web/components/Main/#SideBar/MenuItem/MobileMenuItem.tsx
+++ src/web/components/Main/#SideBar/MenuItem/MobileMenuItem.tsx
@@ -7,17 +7,39 @@ import './styles.scss';
 type MobileMenuItemPropsType = {
   icon: any;
   title: string;
+  submenu?: { title: string; link: string }[] | undefined;
+  isActive: boolean;
+  onClick: () => void;
 };
 
-const MobileMenuItem = ({ icon: Icon, title }: MobileMenuItemPropsType) => {
+const MobileMenuItem = ({
+  icon: Icon,
+  title,
+  submenu,
+  isActive,
+  onClick,
+}: MobileMenuItemPropsType) => {
   return (
-    <li className="menu-item arrow-enabled">
-      <div className="item">
-        <Icon className="logo" />
-        <span className="item-name">{title}</span>
+    <li className="menu-item" onClick={onClick}>
+      <div>
+        <div className="item arrow-enabled">
+          <Icon className="logo" />
+          <span className="item-name">{title}</span>
+        </div>
+        <div className="item-arrow">
+          <IoMdArrowDropdown className="arrow-logo" />
+        </div>
       </div>
-      <div className="item-arrow">
-        <IoMdArrowDropdown className="arrow-logo" />
+      <div>
+        {submenu && (
+          <ul className={isActive ? 'submenu active' : 'submenu inactive'}>
+            {submenu?.map((el) => (
+              <a href={el.link} className="submenu__link">
+                {el.title}
+              </a>
+            ))}
+          </ul>
+        )}
       </div>
     </li>
   );
--- src/web/components/Main/#SideBar/MobileSideBar/Menu.tsx
+++ src/web/components/Main/#SideBar/MobileSideBar/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { MdClear } from 'react-icons/md';
 import { HiOutlineUserCircle } from 'react-icons/hi';
 import { IoMdArrowDropdown } from 'react-icons/io';
@@ -12,28 +12,44 @@ interface MenuProps {
   handleSideBarControl: any;
 }
 
-const Menu: FC<MenuProps> = ({ handleSideBarControl }) => (
-  <>
-    <section className="sidebar-menu__wrapper">
-      <div className="sidebar-menu">
-        <div className="header-menu">
-          <MdClear className="close-logo" onClick={handleSideBarControl} />
-        </div>
-        <ul className="menu-body">
-          {menuItems.map((menuItem) => (
-            <MenuItem icon={menuItem.icon} title={menuItem.title} />
-          ))}
-        </ul>
-        <div className="footer">
-          <div className="user__container">
-            <HiOutlineUserCircle className="user-img" />
-            <div className="username">Username</div>
-            <IoMdArrowDropdown className="arrow-logo" />
+const Menu: FC<MenuProps> = ({ handleSideBarControl }) => {
+  const [isActive, setActive] = useState<number | null>(null);
+
+  return (
+    <>
+      <section className="sidebar-menu__wrapper">
+        <div className="sidebar-menu">
+          <div className="header-menu">
+            <MdClear className="close-logo" onClick={handleSideBarControl} />
+          </div>
+          <ul className="menu-body">
+            {menuItems.map((menuItem, index) => (
+              <MenuItem
+                onClick={() => {
+                  if (isActive !== index) {
+                    setActive(index);
+                  } else {
+                    setActive(null);
+                  }
+                }}
+                icon={menuItem.icon}
+                title={menuItem.title}
+                submenu={menuItem.submenu}
+                isActive={Boolean(isActive === index)}
+              />
+            ))}
+          </ul>
+          <div className="footer">
+            <div className="user__container">
+              <HiOutlineUserCircle className="user-img" />
+              <div className="username">Username</div>
+              <IoMdArrowDropdown className="arrow-logo" />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
 
 export default Menu;
